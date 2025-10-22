8551d25894bd5b9dde2ca991a2347545b6accde7
Bxll45244 <Ccogerbell5071@gmail.com>
2025-10-22 13:59:44 +0700

    fix: reapply .gitignore

--- src/components/golfer/animations/LoadingAnimation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoadingAnimation = () => {
-  return (
-    <span className="loading loading-dots loading-lg"></span>
-  );
-};
-
-export default LoadingAnimation;
