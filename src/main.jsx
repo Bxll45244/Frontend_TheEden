95a7e679d5666bdbe2607a7d58ad5f198a597baf
Bxll45244 <Ccogerbell5071@gmail.com>
2025-11-04 16:14:32 +0700

    fix message-error , router in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import golferRouter from "./routes/golfer/golferRouter";
+import golferRouter from "./routes/golferRouter";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
