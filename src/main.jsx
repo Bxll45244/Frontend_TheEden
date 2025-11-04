f8d5124781859a65c91e841bae5e4e2b8db98090
Bxll45244 <Ccogerbell5071@gmail.com>
2025-11-04 21:10:53 +0700

    fix src/service/ItemService.js

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={Router} />
-      <RouterProvider router={Router} />
+
     </AuthProvider>
   </React.StrictMode>
 );
