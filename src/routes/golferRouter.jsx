bc2e488b74983985379584995c503b7e48cac530
Bxll45244 <Ccogerbell5071@gmail.com>
2025-11-06 02:44:41 +0700

    success

--- src/routes/golferRouter.jsx
+++ src/routes/golferRouter.jsx
@@ -5,8 +5,8 @@ import GolferBookingPage from "../pages/golfer/GolferBookingPage";
 // import StaffLoginPage from "../pages/auth/StaffLoginPage";
 import ProfilePage from "../pages/golfer/ProfilePage";
 import CheckoutSuccess from "../pages/golfer/CheckoutSuccess";
-// import LoginPage from "../pages/auth/LoginPage";
-// import RegisterPage from "../pages/auth/RegisterPage";
+import LoginPage from "../pages/auth/LoginPage";
+import RegisterPage from "../pages/auth/RegisterPage";
 import UnauthorizedPage from "../pages/golfer/UnauthorizedPage";
 
 // router สำหรับ golfer (ผู้ใช้งานทั่วไป)
@@ -27,18 +27,18 @@ const golferRouter = createBrowserRouter([
     path: "/booking/success", // หน้าหลังจากจ่ายเงินเสร็จ
     element: <CheckoutSuccess />,
   },
-  // {
-  //   path: "/login",         // หน้าเข้าสู่ระบบ
-  //   element: <LoginPage />,
-  // },
+  {
+    path: "/login",         // หน้าเข้าสู่ระบบ
+    element: <LoginPage />,
+  },
   // {
   //   path: "/staff/login",   // หน้าเข้าสู่ระบบพนักงาน/ผู้ดูแล
   //   element: <StaffLoginPage />,
   // },
-  // {
-  //   path: "/register",      // หน้าสมัครสมาชิก
-  //   element: <RegisterPage />,
-  // },
+  {
+    path: "/register",      // หน้าสมัครสมาชิก
+    element: <RegisterPage />,
+  },
   {
     path: "/unauthorized",    // หน้าสำหรับคนที่ไม่มีสิทธิ์เข้าถึง
     element: <UnauthorizedPage />,
